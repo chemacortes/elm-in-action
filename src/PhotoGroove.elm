a6dd4f6986add6252e45663fbad8ab2d1615f053
chemacortes <devxtrem@gmail.com>
2020-06-17 21:18:03 +0200

    Firsts interactions

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,23 +1,41 @@
 module PhotoGroove exposing (main)
 
+import Browser
 import Html exposing (Html, div, h1, img, text)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+
+
+type alias Thumb =
+    { url : String }
 
 
 type alias Model =
-    List { url : String }
+    { photos : List Thumb
+    , selectedUrl : String
+    }
 
 
 type alias Msg =
-    String
+    { description : String
+    , data : String
+    }
 
 
 initialModel : Model
 initialModel =
-    [ { url = "1.jpg" }
-    , { url = "2.jpg" }
-    , { url = "3.jpg" }
-    ]
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
+
+
+urlPrefix : String
+urlPrefix =
+    "http://elm-in-action.com/"
 
 
 view : Model -> Html Msg
@@ -26,13 +44,35 @@ view model =
         [ div [ class "content" ]
             [ h1 [] [ text "Photo Groove" ] ]
         , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/2.jpeg" ] []
-            , img [ src "http://elm-in-action.com/3.jpeg" ] []
-            ]
+            (List.map (viewThumbnail model.selectedUrl) model.photos)
+        , img [ class "large", src (urlPrefix ++ "large/" ++ model.selectedUrl) ]
+            []
         ]
 
 
-main : Html Msg
+viewThumbnail : String -> Thumb -> Html Msg
+viewThumbnail selectedUrl thumb =
+    img
+        [ src (urlPrefix ++ thumb.url)
+        , classList [ ( "selected", selectedUrl == thumb.url ) ]
+        , onClick { description = "ClickedPhoto", data = thumb.url }
+        ]
+        []
+
+
+update : Msg -> Model -> Model
+update msg model =
+    if msg.description == "ClickedPhoto" then
+        { model | selectedUrl = msg.data }
+
+    else
+        model
+
+
+main : Program () Model Msg
 main =
-    view initialModel
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
