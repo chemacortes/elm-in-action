e5a5aed487194a97be3cfa0c55b7f427f6193e89
chemacortes <devxtrem@gmail.com>
2022-07-30 18:08:46 +0200

    Testing sliders update

--- tests/PhotoGrooveTests.elm
+++ tests/PhotoGrooveTests.elm
@@ -4,8 +4,8 @@ import Expect
 import Fuzz exposing (Fuzzer, int, list, string)
 import Json.Decode as Decode exposing (decodeValue)
 import Json.Encode as Encode
-import PhotoGroove exposing (Msg(..), initialModel, update)
-import Test exposing (Test, fuzz, fuzz2, test)
+import PhotoGroove exposing (Model, Msg(..), initialModel, update)
+import Test exposing (Test, describe, fuzz, fuzz2, test)
 
 
 suite : Test
@@ -37,4 +37,24 @@ slidHueSetsHue =
                 |> update (SlidHue amount)
                 |> Tuple.first
                 |> .hue
+                |> Expect.equal amount
+
+
+sliders : Test
+sliders =
+    describe "Slider sets the desired field in the Model"
+        [ testSlider "SlidHue" SlidHue .hue
+        , testSlider "SlidRipple" SlidRipple .ripple
+        , testSlider "SlidNoise" SlidNoise .noise
+        ]
+
+
+testSlider : String -> (Int -> Msg) -> (Model -> Int) -> Test
+testSlider description toMsg amountFromModel =
+    fuzz int (description ++ " sets the hue") <|
+        \amount ->
+            initialModel
+                |> update (toMsg amount)
+                |> Tuple.first
+                |> amountFromModel
                 |> Expect.equal amount
